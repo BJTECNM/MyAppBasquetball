22ca6625aa362f854f04118f79435147eef8b906
BJ <192t0220@itsm.edu.mx>
2023-10-07 18:07:42 -0600

    git commit

--- app/src/main/java/com/example/myappbasquetball/MainActivity.kt
+++ app/src/main/java/com/example/myappbasquetball/MainActivity.kt
@@ -4,10 +4,46 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.myappbasquetball.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
+
+        viewModel.localScore.observe(this, Observer {
+            binding.localScoreText.text = it.toString()
+        })
+
+        viewModel.visitorScore.observe(this, Observer {
+            binding.visitorScoreText.text = it.toString()
+        })
+
+        binding.resultsButton.setOnClickListener {
+            startScoreActivity()
+        }
+    }
+
+    private fun startScoreActivity() {
+        val intent = Intent(this, ScoreActivity::class.java)
+        intent.putExtra(ScoreActivity.LOCAL_SCORE_KEY, viewModel.localScore.value)
+        intent.putExtra(ScoreActivity.VISITOR_SCORE_KEY, viewModel.visitorScore.value)
+        startActivity(intent)
+    }
+
+    /*
     private lateinit var binding : ActivityMainBinding
 
     private lateinit var viewModel: MainViewModel
@@ -72,4 +108,5 @@ class MainActivity : AppCompatActivity() {
         intent.putExtra(ScoreActivity.VISITOR_SCORE_KEY, viewModel.visitorScore)
         startActivity(intent)
     }
+     */
 }
